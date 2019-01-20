927b9d7aeaeea5e60932b87399b1b26ab4f74035
DCU4 <DylanJConnor4@gmail.com>
2019-01-20 03:40:42 -0500

    pick your hue!

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ var scale =document.querySelector('.scale');
 var myColor = document.getElementById("myColor");
     var myLight = document.getElementById("myLight");
 
+var colorDisplay = document.getElementById("colorDisplay");
+
 // var clearBtn = document.getElementById('clear-canvas');
 // console.log(clearBtn);
 
@@ -106,11 +108,8 @@ function sketchpad_mouseMove(e) {
     if (mouseDown==1) {
         console.log('1');
         drawLine(ctx,mouseX,mouseY,slider.value);
-    } else {
-        console.log('no');
-    }
+    } 
 }
-
 // Get the current mouse position relative to the top-left of the canvas
 function getMousePos(e) {
     if (!e)
@@ -126,26 +125,51 @@ function getMousePos(e) {
     }
 }
 
+
 // DRAW ___________________________
+    
+// var h1=myColor.value,  l1=myLight.value;
+// console.log(myColor.value);
+
+
+myColor.addEventListener('input',function(e) {
+    colorDisplay.style.background = '#000';
+    var displayColor = "hsl("+myColor.value+",100%,"+myLight.value+"%)";
+    colorDisplay.style.background = displayColor;
+    console.log(myLight.value)
+    console.log(displayColor);
+
+});
 
 // Draws a dot at a specific position on the supplied canvas name
 // Parameters are: A canvas context, the x position, the y position
 function drawLine(ctx,x,y,size) {
     // Let's use black by setting RGB values to 0, and 255 alpha (completely opaque)
-    r=0; g=0; b=0; a=255;
+    // r=110; g=200; b=220; a=255;
 
 
     
     
-    h=myColor.value; s=0; l=myLight.value;
-    console.log(h, l);
-    lineColor = "hsl("+h+","+s+","+l+")";
-    console.log(lineColor);
+    
+    // lineColor = "hsl(200,100%,50%)";
+    
+    
     // // Select a fill style
-    // ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
-    // ctx.fillStyle = color.value;
-    ctx.fillStyle = lineColor;
+    var h=myColor.value,  l=myLight.value;
+    lineColor = "hsl("+h+",100%,"+l+"%)";
+    console.log(myColor.value);
+    ctx.strokeStyle = lineColor;
 
+    colorDisplay.style.background = lineColor;
+
+    myColor.addEventListener('oninput',function() {
+        colorDisplay.style.background = lineColor;
+        console.log(colorDisplay);
+    });
+
+    console.log(colorDisplay.style.backgroundColor);
+    // ctx.fillStyle = '#00ff00';
+    console.log(ctx.fillStyle);
     // If lastX is not set, set lastX and lastY to the current position
     if (lastX==-1) {
         lastX=x;
