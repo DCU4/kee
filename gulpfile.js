eeb8f4365a1ac0869b5a063dc988b6873ca216f7
DCU4 <DylanJConnor4@gmail.com>
2019-02-24 21:17:08 -0500

    update sass; create mixins and variables for app

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify'),
     concat = require('gulp-concat');
+// var notify = require('gulp-notify');
 
 gulp.task('log', function(done) {
 gutil.log('== My Log Task ==');
@@ -10,12 +11,24 @@ done();
 });
 
 gulp.task('sass', function(done) {
-gulp.src('style.scss')
+gulp.src('sass/**/*.scss')
 .pipe(sass({style: 'expanded'}))
     .on('error', gutil.log)
 .pipe(gulp.dest('css'));
 done();
 });
+
+// gulp.task('sass', function() {
+//     return gulp.src('sass/**/*.scss')
+//     .pipe(sass().on("error", notify.onError("Error: <%= error.message %>")))
+//     .pipe(autoprefixer({
+//         browsers: ['last 2 versions', 'ie >= 11'],
+//         cascade: false
+//     }))
+//     .pipe(sourcemaps.init())
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest('css'));
+//   });
 
 gulp.task('js', function(done) {
 gulp.src('main.js')
@@ -27,6 +40,6 @@ done();
 
 gulp.task('watch', function(done) {
 gulp.watch('main.js', gulp.series('js'));
-gulp.watch('style.scss', gulp.series('sass'));
+gulp.watch('sass/**/*.scss', gulp.series('sass'));
 done();
 });
