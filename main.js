7945a681da43a4d589c337f3887fcc7708c434a1
DCU4 <DylanJConnor4@gmail.com>
2019-03-09 15:58:29 -0500

    fix touch placement for drawing to be more accurate

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ menuBtn.addEventListener('click', function(){
             if (e.touches.length == 1) { // Only deal with one finger
                 var touch = e.touches[0]; // Get the information for finger #1
                 touchX=touch.pageX-touch.target.offsetLeft;
-                touchY=touch.pageY-touch.target.offsetTop;
+                touchY=touch.pageY-touch.target.offsetTop-40;
             }
         }
     }
