23fc2c9694c426e84656fb86cb69efd50b1de4e1
DCU4 <DylanJConnor4@gmail.com>
2019-03-24 15:46:58 -0400

    edit touch position

--- public/js/main.js
+++ public/js/main.js
@@ -99,7 +99,8 @@ menuBtn.addEventListener('click', function(){
             if (e.touches.length == 1) { // Only deal with one finger
                 var touch = e.touches[0]; // Get the information for finger #1
                 touchX=touch.pageX-touch.target.offsetLeft;
-                touchY=touch.pageY-touch.target.offsetTop-40;
+                touchY=touch.pageY-touch.target.offsetTop-65;
+                // touchY=touch.pageY-touch.target.offsetTop-40;
             }
         }
     }
