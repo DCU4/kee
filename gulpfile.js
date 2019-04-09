b622d01e2412d95b7670c07c3b538d196217c7c8
Dylan Connor <dconnor@req.co>
2019-04-09 16:12:41 -0400

    update gulp with browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,49 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync').create();
+    var reload      = browserSync.reload;
 
 gulp.task('log', function(done) {
-gutil.log('== My Log Task ==');
-done();
+    gutil.log('== My Log Task ==');
+    done();
+});
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server: "./"
+        // or
+        // proxy: 'index.html'
+    });
+    gulp.watch('sass/**/*.scss').on('change',reload);
+
 });
 
 gulp.task('sass', function(done) {
-gulp.src('sass/**/*.scss')
-.pipe(sass({style: 'expanded'}))
-    .on('error', gutil.log)
-.pipe(gulp.dest('public/css'));
-done();
+
+    gulp.src('sass/**/*.scss')
+    .pipe(sass({style: 'expanded'}))
+        .on('error', gutil.log)
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream());
+    done();
 });
 
 gulp.task('js', function(done) {
-gulp.src('js/*.js')
-.pipe(uglify().on('error', function(e){
-    console.log(e);
-    }))
-.pipe(concat('script.js'))
-.pipe(gulp.dest('public/scripts'));
-done();
+    gulp.src('js/*.js')
+    .pipe(uglify().on('error', function(e){
+        console.log(e);
+        }))
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('public/scripts'));
+    done();
 });
 
 gulp.task('watch', function(done) {
-gulp.watch('js/*.js', gulp.series('js'));
-gulp.watch('sass/**/*.scss', gulp.series('sass'));
-done();
+    gulp.watch('js/*.js', gulp.series('js'));
+    gulp.watch('sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('sass/**/*.scss',gulp.series('browser-sync'));
+    done();
 });
