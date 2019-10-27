3eecf2f83273d8ca38d8d6987ebd7f8b453b64c4
Dylan Connor <DylanJConnor4@gmail.com>
2019-10-27 16:37:43 -0400

    creating A2HS element; functionality sort of

--- js/main.js
+++ js/main.js
@@ -588,11 +588,38 @@ if (document.getElementById('sketchpad')){
 }
 
 var deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', function(e) {
+console.log(deferredPrompt);
+window.addEventListener('beforeinstallprompt', function(event) {
   // Stash the event so it can be triggered later.
   console.log(e);
+  e.preventDefault();
   deferredPrompt = e;
-  deferredPrompt.prompt();
+  // e.prompt();
+  // showInstallPromotion();
   // ...
+  // 
+});
+
+
+
+// function showInstallPromotion(){
+//   deferredPrompt.prompt()
+// };
+// do we auto show prompt or have user decide?
+
+btnAdd.addEventListener('click', function(e) {
+  // hide our user interface that shows our A2HS button
+  // btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then(function(choiceResult)  {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
 });
