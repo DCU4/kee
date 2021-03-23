19bb490eac9769a8a564497c6ef66ffacb6bad06
Dylan Connor <DylanJConnor4@gmail.com>
2021-03-22 20:17:57 -0400

    update bodyParser to built in express module

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
 app.use(require("express-session")({
