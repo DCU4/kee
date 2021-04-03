a3a16c64cc1fb05573b40fab4637bddc037b1007
Dylan Connor <DylanJConnor4@gmail.com>
2021-04-03 18:29:21 -0400

    add created value to user db schema

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 
 var userSchema = new mongoose.Schema({
+    created: {type: Date, default: Date.now},
     username: String,
     password: String,
     savedColors: []
